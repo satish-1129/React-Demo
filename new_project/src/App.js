c9a5067eba061edc40c74496846b7fc35ad59253
satish-1129 <satishpatani935@gmail.com>
2024-07-26 16:00:55 +0530

    added new file in second branch

--- new_project/src/App.js
+++ new_project/src/App.js
@@ -1,10 +1,12 @@
 import Attendance from "./Attendance";
 import Classes from "./Classes";
+import Placements from "./placements";
 function App() {
   return (
     <>
     <Classes/>
     <Attendance/>
+    <Placements/>
     </>
   );
 }
